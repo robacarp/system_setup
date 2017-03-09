2be8f4e6259e48e09065b5329ac7d34da5d9be48
robacarp <robacarp@users.noreply.github.com>
2017-03-08 21:03:25 -0700

    Update casks.sh

--- casks.sh
+++ casks.sh
@@ -3,32 +3,38 @@
 # Install cask'd applications
 brew cask install 1password
 brew cask install adium
+brew cask install alfred
 brew cask install bartender
 brew cask install caffeine
+brew cask install choosy
+brew cask install dash
 brew cask install dropbox
   echo 'open /Applications/Dropbox.app' >> $notes_file
-brew cask install dash
+brew cask install firefox
 brew cask install flux
+brew cask install gitx
 brew cask install google-chrome
-brew cask install github-desktop
+brew cask install gpgtools
 brew cask install hammerspoon
+brew cask install hex-fiend
 brew cask install istat-menus
-brew cask install iterm2
 brew cask install java
 brew cask install karabiner
-brew cask install knock
 brew cask install launchrocket
 brew cask install macvim
 brew cask install ngrok
+brew cask install omnifocus
 brew cask install paw
 brew cask install postico
+brew cask install quicksilver
 brew cask install screenhero
+brew cask install sequel-pro
 brew cask install slack
-brew cask install tunnelblick
 brew cask install telegram
+brew cask install tunnelblick
 brew cask install vlc
 brew cask install xquartz
-brew cask install quicksilver
+
 
 # applications that still need to be installed
 # - evoluent mouse software
